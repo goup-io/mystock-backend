e5939ad64063455ce54b1cc31c88b5c012d975c4
joao-noleto13 <jpnoleto1@gmail.com>
2024-05-10 23:23:27 -0300

    Featch : Csv estoque e mudança de nome arquivo e endpoints

--- src/main/java/com/goup/repositories/produtos/ETPRepository.java
+++ src/main/java/com/goup/repositories/produtos/ETPRepository.java
@@ -4,10 +4,14 @@ import com.goup.entities.estoque.ETP;
 import com.goup.entities.estoque.Tamanho;
 import com.goup.entities.estoque.produtos.Produto;
 import com.goup.entities.lojas.Loja;
+import com.goup.entities.usuarios.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ETPRepository extends JpaRepository<ETP, Integer>{
     Optional<ETP> findByTamanhoAndLojaAndProduto(Tamanho tamanho, Loja loja, Produto produto);
+
+    List<ETP> findAllByLoja(Loja loja);
 }
