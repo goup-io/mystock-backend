9d3c2382c67fcfe8cc4e04cce742fc711084112b
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2024-06-08 02:05:03 -0300

    fix: ajuste na query de filtro de venda, relacionados as datas

--- src/main/java/com/goup/repositories/vendas/VendaRepository.java
+++ src/main/java/com/goup/repositories/vendas/VendaRepository.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
     List<Venda> findAllByUsuarioLojaIdAndStatusVendaStatus(Integer lojaId, StatusVenda.Status status);
-    
+
     @Query("SELECT venda FROM Venda venda " +
             "WHERE (:tipo_venda IS NULL OR venda.tipoVenda = :tipo_venda) " +
             "AND (:id_vendedor IS NULL OR venda.usuario.id = :id_vendedor)" +
-            "AND (venda.dataHora BETWEEN :data_inicio AND :data_fim)" +
+            "AND (:data_inicio IS NULL OR :data_fim IS NULL OR venda.dataHora BETWEEN :data_inicio AND :data_fim)" +
             "AND (:id_loja IS NULL OR venda.usuario.loja.id = :id_loja)"
     )
     List<Venda> findAllByFiltros(
