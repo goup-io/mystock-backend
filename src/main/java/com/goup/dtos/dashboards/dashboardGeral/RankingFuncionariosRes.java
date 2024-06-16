7b50fddba92db28d96909af8ddd9cda12c622aa5
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2024-06-16 15:14:49 -0300

    fix: adicionando a service

--- src/main/java/com/goup/dtos/dashboards/dashboardGeral/RankingFuncionariosRes.java
+++ src/main/java/com/goup/dtos/dashboards/dashboardGeral/RankingFuncionariosRes.java
@@ -1,2 +1,4 @@
-package com.goup.dtos.dashboards.dashboardGeral;public record RankingFuncionariosRes() {
+package com.goup.dtos.dashboards.dashboardGeral;
+
+public record RankingFuncionariosRes(Integer idFuncionario, String nomeFuncionario, Double valorVendido) {
 }
