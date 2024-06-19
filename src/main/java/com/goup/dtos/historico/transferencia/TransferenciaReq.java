748776020ddcf8bde973a0a6a548d8e25325724b
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2024-06-19 12:12:39 -0300

    fix: alterando para receber codigoVenda ao inves de id nas transferencias

--- src/main/java/com/goup/dtos/historico/transferencia/TransferenciaReq.java
+++ src/main/java/com/goup/dtos/historico/transferencia/TransferenciaReq.java
@@ -3,7 +3,7 @@ package com.goup.dtos.historico.transferencia;
 import java.util.List;
 
 public record TransferenciaReq(
-        Integer coletor_id,
+        Integer coletor_cod,
         List<TransferenciaETPEQuantidade> itens
 ) {
 }
