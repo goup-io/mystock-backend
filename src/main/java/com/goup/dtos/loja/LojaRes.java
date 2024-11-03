aa0630998c6f72292ac2252716a307bb0a6aef21
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2024-11-03 15:00:25 -0300

    feat: removido tudo relacionado a endereço/viacep

--- src/main/java/com/goup/dtos/loja/LojaRes.java
+++ src/main/java/com/goup/dtos/loja/LojaRes.java
@@ -1,22 +1,5 @@
 package com.goup.dtos.loja;
 
-import com.goup.dtos.loja.endereco.Endereco;
-import com.goup.dtos.loja.endereco.EnderecoMapper;
-import com.goup.dtos.loja.endereco.EnderecoRes;
-import com.goup.services.ViacepService;
-
-import java.io.IOException;
-
 public record LojaRes(Integer id, String nome, String cnpj, String cep, Integer numero, String complemento){
-    public EnderecoRes getEndereco(){
-        ViacepService viaCep = new ViacepService();
-        try{
-            Endereco endereco = viaCep.getEndereco(this.cep());
-            return EnderecoMapper.toDto(endereco);
-        } catch(IOException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
 
--- src/main/java/com/goup/dtos/loja/endereco/Endereco.java
+++
@@ -1,6 +0,0 @@
-package com.goup.dtos.loja.endereco;
-
-
-public record Endereco(String cep, String logradouro, String complemento, String bairro, String uf, String ibge, String gia, String ddd, String siafi){
-
-}
